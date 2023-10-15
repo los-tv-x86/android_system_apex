d2f3236fc6fa7b92d04401efac07dfce788913bf
Michael Goffioul <michael.goffioul@gmail.com>
2023-10-15 18:34:57 -0400

    Allow APEX loop-mount from TMPFS
    AOSP uses special unbeffered access to mount an APEX image, and only
    allow fall back to buffered acces on specific cases: erofs and suqashfs.
    Add tmpfs to the allowed buffered access, so APEX can be mounted from
    /data partition in LIVE mode.
    
    Change-Id: Iaebc8c5f8666fc01d4b2450b35989a25064a0687

--- apexd/apexd_loop.cpp
+++ apexd/apexd_loop.cpp
@@ -402,6 +402,7 @@ static Result<LoopbackDeviceUniqueFd> ConfigureLoopDevice(
     if (statfs(target.c_str(), &stbuf) != 0 ||
         (stbuf.f_type != EROFS_SUPER_MAGIC_V1 &&
          stbuf.f_type != SQUASHFS_MAGIC &&
+         stbuf.f_type != TMPFS_MAGIC &&
          stbuf.f_type != OVERLAYFS_SUPER_MAGIC)) {
       return Error(saved_errno) << "Failed to open " << target;
     }
